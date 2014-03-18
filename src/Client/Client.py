07b99a759ca99c65b4a4fac21c6161020e408f58
SilasAlroe <silasalroe@gmail.com>
2014-03-19 00:41:23 +0100

    Cleaned up the code a bit

--- src/Client/Client.py
+++ src/Client/Client.py
@@ -18,22 +18,18 @@ class LoginScreen(Screen):
         self.whilelogin = Popup(title='Logging in',content=Label(text='Please wait'),auto_dismiss=False,size_hint=(0.5, 0.5))
         self.whilelogin.open()
         App.get_running_app().ccommunication.login(username, password)
-        timeoutcountdown=12
-        while (timeoutcountdown > 0):
-            print "Trying to reach server. Fail in: "+str(timeoutcountdown)
+        loginsuccess=False
+        for x in range(12):
+            print "Trying to reach server. Fail in: "+str(12-x)
             if (App.get_running_app().receiver.loggedin):
                 loginsuccess=True
                 break
             sleep(0.5)
-            timeoutcountdown-=1
-        else:
-            loginsuccess=False
+        self.whilelogin.dismiss()
         if (loginsuccess):
             print "Successfully logged in as" +username
             App.get_running_app().sm.current = "friends" #use App.get_running_app() to access current app
-            self.whilelogin.dismiss()
         else:
-            self.whilelogin.dismiss()
             loginfailbutton = Button(text='Please:\nCheck your connection\nCheck your login details\nTry again')
             self.loginfail = Popup(title='Login failed',content=loginfailbutton,auto_dismiss=False,size_hint=(0.5, 0.5))
             loginfailbutton.bind(on_press=self.loginfail.dismiss)
