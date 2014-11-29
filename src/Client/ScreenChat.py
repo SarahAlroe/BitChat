4ff013ab26ac40b03766fa6c665352f97624b04c
Silas Alroe <silasalroe@gmail.com>
2014-11-29 23:03:20 +0100

    Made text delete itself after sending.

--- src/Client/ScreenChat.py
+++ src/Client/ScreenChat.py
@@ -5,6 +5,9 @@ class ScreenChat(Screen):
     def on_pre_enter(self):
         App.get_running_app().chatBox=self.ids.chatBox.text
     def sendMessage(self,text):
-        App.get_running_app().ccommunication.sendmsg(App.get_running_app().dict.currentPartner, text)
-        self.ids.chatBox.text += "\n"+App.get_running_app().username+": "+text
+        if text != "":
+            App.get_running_app().ccommunication.sendmsg(App.get_running_app().dict.currentPartner, text)
+            self.ids.chatBox.text += "\n"+App.get_running_app().username+": "+text
+            self.ids.sendBox.text = ""
+
     pass
